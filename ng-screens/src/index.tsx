726f071ee4f6bb7796c8c22358531a63c289b09e
oliveiramiro2 <ramiromares95@gmail.com>
2022-12-04 18:37:50 -0300

    added lib to use navigation and styled components to make components styles

--- ng-screens/src/index.tsx
+++ ng-screens/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import SingIn from "./screens/public/SingIn";
 import reportWebVitals from "./reportWebVitals";
 
