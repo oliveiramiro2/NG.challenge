1ead303837f5f1a852910bbb33600f807dfed86c
oliveiramiro2 <ramiromares95@gmail.com>
2022-12-06 19:38:55 -0300

    added feedback to user when your balance is not come from request

--- ng-screens/src/screens/private/Home/index.tsx
+++ ng-screens/src/screens/private/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext, useState } from "react";
 import ScrollAnimation from "react-animate-on-scroll";
+import { Store } from "react-notifications-component";
 
 import {
     SContain,
@@ -18,16 +19,39 @@ const HomePrivate: React.FC = () => {
     const { userData } = useContext(AuthContext);
     const [dataRecovered, setDataRecovered] = useState<boolean>(false);
 
+    const alertMessage = (msg: string, type: string) => {
+        const typeOfAlert = type === "danger" ? "danger" : "success";
+        Store.addNotification({
+            message: `${msg}`,
+            type: `${typeOfAlert}`,
+            container: "center",
+            insert: "top",
+            animationIn: ["animate__animated", "animate__zoomIn"],
+            animationOut: ["animate__animated", "animate__zoomOut"],
+            dismiss: {
+                duration: 5000,
+                onScreen: true,
+            },
+        });
+    };
+
     useEffect(() => {
         document.title = "NG challenge - Home";
 
-        if(!dataRecovered) {
+        if (!dataRecovered) {
             api.get("/account", {
                 headers: { token: localStorage.getItem("token") },
-            }).then(({data}) => {
-                setBalance(data.balance)
-                setDataRecovered(true)
-            }).catch(() => {});
+            })
+                .then(({ data }) => {
+                    setBalance(data.balance);
+                    setDataRecovered(true);
+                })
+                .catch(() => {
+                    alertMessage(
+                        "Desculpe, não foi possível recurar seu saldo.",
+                        "danger"
+                    );
+                });
         }
     }, []);
 
@@ -40,7 +64,9 @@ const HomePrivate: React.FC = () => {
                 </ScrollAnimation>
                 <article>
                     <SImgHome alt="imagem-carteira" src={imgHome} />
-                    <STitleUserName>{`Saldo: R$ ${balance.toFixed(2)}`}</STitleUserName>
+                    <STitleUserName>{`Saldo: R$ ${balance.toFixed(
+                        2
+                    )}`}</STitleUserName>
                 </article>
             </SContainerContent>
         </SContain>
